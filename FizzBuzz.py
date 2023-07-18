e936ccb1262f735f57b2361b656958510a1cad29
Adham Sameh <139157688+Adham20000@users.noreply.github.com>
2023-07-19 02:25:40 +0300

    new Jupiter notebook added

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+for num in range (1,101):
+  if num%3 == 0 and num%5 == 0:
+    print("FizzBuzz")
+  elif num%3 == 0:
+    print("Fizz")
+  elif num%5 == 0:
+    print("Buzz")
+  else:
+    print(num)   
+
